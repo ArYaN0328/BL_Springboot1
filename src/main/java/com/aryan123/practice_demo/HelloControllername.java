ccd35a02682961e7d42838c4b0503246163ea15e
ArYaN0328 <121503458+ArYaN0328@users.noreply.github.com>
2025-03-03 16:57:09 +0530

    Changes made

--- src/main/java/com/aryan123/practice_demo/HelloControllername.java
+++ src/main/java/com/aryan123/practice_demo/HelloControllername.java
@@ -1,17 +1,21 @@
 package com.aryan123.practice_demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+
 @RestController
 @RequestMapping("/hello")
-public class HelloController2 {
 
-   @GetMapping("/query")
-    public String sayhello(@RequestParam String name)
-   {
-       return "Hello," +name+" from Bridgelabz";
-   }
+public class HelloControllername {
+
+    @GetMapping("/control/{name}")
+    public String sayHello(@PathVariable String name)
+    {
+        return "hello"+name+" from BridgeLabz";
+    }
+
 
 }
