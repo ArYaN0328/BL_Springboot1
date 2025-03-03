370be7b3a7a54a91d60b69682704ae1ffa1428ee
ArYaN0328 <121503458+ArYaN0328@users.noreply.github.com>
2025-03-03 15:08:03 +0530

    Boilerplate main

---
+++ src/test/java/com/aryan123/practice_demo/PracticeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aryan123.practice_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
